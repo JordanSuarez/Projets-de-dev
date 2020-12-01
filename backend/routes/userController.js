1b363cbf58500ec4156602f12182888cefdc8244
Almigra <sahar.almigra@gmail.com>
2020-12-01 14:24:33 +0100

    replaced name by username, and added email and password regex 🤠

--- backend/routes/userController.js
+++ backend/routes/userController.js
@@ -3,17 +3,34 @@ const bcrypt = require('bcrypt');
 const jwtUtils = require('../utils/jwt.utils');
 const models = require('../models');
 
+//constantes
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PASSWORD_REGEX = /^(?=.*\d).{4,15}$/;
+
 //Routes
 module.exports = {
     register: (req, res) => {
-      const name = req.body.name;
+      const username = req.body.username;
       const email = req.body.email;
 	  	const password = req.body.password;
 			
-			if(email == null || name == null || password == null) {
-				return res.status(400).json({'error': 'missing parameters'});
+			if(email == null || username == null || password == null) {
+				return res.status(400).json({'error': 'Tous les champs requis ne sont pas renseignés'});
+			}
+
+			if (username.length >= 13 || username.length <=4) {
+				return res.status(400).json({'error': 'Le nom d\'utilisateur est invalide, la longueur doit être comprise entre 5 et 12 caractères'});
+			}
+
+			if (!EMAIL_REGEX.test(email)) {
+				return res.status(400).json({'error': 'L\'email saisi est invalide'})
 			}
 			// TODO Faire les vérifications de longueur, caractères etc.
+
+			if (!PASSWORD_REGEX.test(password)) {
+				return res.status(400).json({'error': 'La longueur du mot de passe doit être comprise entre 4 et 15 caractères et doit contenir au moins un caractère numérique'})
+			}			
+			
 			models.User.findOne({
 				attributes: ['email'],
 				where: { email: email}
@@ -23,7 +40,7 @@ module.exports = {
 					bcrypt.hash(password, 5, (err, bcryptedPassword ) => {
 						const newUser = models.User.create({
 							email:email,
-							name:name,
+							username:username,
 							password: bcryptedPassword,
 							isAdmin: 0
 						})
@@ -33,24 +50,18 @@ module.exports = {
               })
             })
             .catch((err) => {
-              return res.status(500).json({'error': 'cannot add user'})
+              return res.status(500).json({'error': 'le nom d\'utilisateur et/ou l\'email est déjà utilisé'})
             })
 					})
 				} else {
-					return res.status(409).json({ 'error': 'user already exist'});
+					return res.status(409).json({ 'error': 'le nom d\'utilisateur et/ou l\'email est déjà utilisé'});
 				}
 			})
 			.catch((err) => {
-				return res.status(500).json({ 'error': 'enable to verify user'});
+				return res.status(500).json({ 'error': 'Erreur lors de la verification de l\'utilisateur'});
 			});
     },
     login: (req, res) => {
-			const email = req.body.email;
-			const password = req.body.password;
-			
-			if (email == null ||  password == null) {
-				return res.status(400).json({ 'error': 'missing parameters' });
-			}
 
 			// TODO Faire les vérifications de longueur, caractères etc.
 
@@ -66,15 +77,15 @@ module.exports = {
 								'token': jwtUtils.generateTokenForUser(userFound)
 							});
 						} else {
-							return res.status(403).json({ 'error': 'invalid password' });
+							return res.status(403).json({ 'error': 'Mot de passe invalide' });
 						}
 					});
 				} else {
-					return res.status(404).json({ 'error': 'user not exist in DataBase'});
+					return res.status(404).json({ 'error': 'L\'utilisateur n\'existe pas dans la base de donnée'});
 				}
 			})
 			.catch((err) => {
-				return res.status(500).json({'error': 'enable to verify user'});
+				return res.status(500).json({'error': 'Impossible de vérifier l\'utilisateur'});
 			});
     },
 }
