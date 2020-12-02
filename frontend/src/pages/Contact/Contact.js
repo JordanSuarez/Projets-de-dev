f6fd6b76c6c785b31fe2eabcbac495f59c405883
GomezCeline <Celinegmz9@gmail.com>
2020-12-02 11:57:57 +0100

    add form contact

--- frontend/src/pages/Contact/Contact.js
+++ frontend/src/pages/Contact/Contact.js
@@ -4,12 +4,71 @@ import Base from 'src/common/components/Base';
 
 // eslint-disable-next-line arrow-body-style
 const Contact = () => {
+  const [formContactValues, setFormContactValues] = useState();
+
+  const onSubmit = (values) => {
+    setFormContactValues(values);
+  };
   return (
     <Base>
-      <div> Contact </div>
+      <div className={classes.container}>
+        <Box borderRadius={16} className={classes.content} boxShadow={2}>
+          <h2 className={classes.formTitle}>Connexion</h2>
+          <Form
+            className={classes.form}
+            onSubmit={onSubmit}
+            initialValues=""
+            validate={validate}
+            render={({ handleSubmit, submitting }) => (
+              <form onSubmit={handleSubmit} noValidate>
+                <TextField
+                  type="text"
+                  label="Name"
+                  name="name"
+                  margin="none"
+                  required={true}
+                />
+                <TextField
+                  type="email"
+                  label="Email"
+                  name="email"
+                  margin="none"
+                  required={true}
+                />
+                <TextField
+                  type="text"
+                  label="Site internet"
+                  name="website"
+                  margin="none"
+                  required={true}
+                />
+                <TextField
+                  type="password"
+                  label="Password"
+                  name="password"
+                  margin="none"
+                  required={true}
+                />
+                <Box className={classes.containerButton}>
+                  <Button
+                    className={classes.submit}
+                    variant="contained"
+                    type="submit"
+                    disabled={submitting}
+                  >
+                    Submit
+                  </Button>
+                  <p className={classes.inscription} onClick="#"> Pas de compte ? Je veux m'inscrire </p>
+                </Box>
+              </form>
+            )}
+          />
+        </Box>
+      </div>
     </Base>
   );
 };
+
 
 Contact.propTypes = {
 
