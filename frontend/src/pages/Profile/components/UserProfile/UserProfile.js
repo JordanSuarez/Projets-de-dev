4251cac58444d7a1be8bc2ae7c8f38e9be983b0c
GomezCeline <Celinegmz9@gmail.com>
2020-12-04 11:45:09 +0100

    modificationprofile page

--- frontend/src/pages/Profile/components/UserProfile/UserProfile.js
+++ frontend/src/pages/Profile/components/UserProfile/UserProfile.js
@@ -1,28 +1,55 @@
 import React from 'react';
-import { getEditionProfileRoute } from 'src/common/routing/routesResolver';
+import { getEditionProfileRoute, getCreationProjectRoute } from 'src/common/routing/routesResolver';
 import { useHistory } from 'react-router-dom';
 import { classes as classesProps } from 'src/common/classes';
 import {
-  Box,
+  Avatar,
   Button,
 } from '@material-ui/core';
+
 import Base from 'src/common/components/Base';
+import avatar2 from './avatar.png';
 
 // eslint-disable-next-line arrow-body-style
 const UserProfile = ({classes}) => {
   const history = useHistory();
-  const handleClick = () => history.push(getEditionProfileRoute());
+  const editProfile = () => history.push(getEditionProfileRoute());
+  const newProject = () => history.push(getCreationProjectRoute());
   return (
     <Base>
-      <div> Profile </div>
-      <Button
-        className={classes.edit}
-        variant="contained"
-        type="button"
-        onClick={handleClick}
-      >
-        Editer
-      </Button>
+      <div className={classes.container}>
+        <h2 className={classes.subtitle}> Mon profil </h2>
+        <div className={classes.column}>
+          <div className={classes.rowCenter}>
+            <Avatar alt="lePseudo" src={avatar2} className={classes.large} />
+            <h3 className={classes.username}>TODO username</h3>
+          </div>
+          <div className={classes.rowCenter}>
+            <Button
+              className={classes.button}
+              variant="contained"
+              type="button"
+              onClick={editProfile}
+            >
+              Editer
+            </Button>
+            <Button
+              className={classes.button}
+              variant="contained"
+              type="button"
+              onClick={newProject}
+            >
+              Ajouter un projet
+            </Button>
+          </div>
+        </div>
+        <div>
+        <h2 className={classes.subtitle}>  
+          Liste des projet
+        </h2>
+        
+        </div>
+      </div>
     </Base>
   );
 };
