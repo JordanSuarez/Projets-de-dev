3448fa9f8095911ab835479398f56d1d262d8f86
Jordan MacBook <suarez.jordan@orange.fr>
2020-12-04 21:42:08 +0100

    Created .env file to save api url information. Installed dotenv-webpack plugin to use .env variable as global variable. Added endpoints file for centralize all api endpoints. Used dynamic axios call on Project page

---
+++ frontend/src/common/callApiHandler/endpoints.js
@@ -0,0 +1,80 @@
+import {
+  GET,
+  POST,
+  PATCH,
+  DELETE,
+  USERS,
+  PROJECTS,
+  CONTACT,
+  LOGIN,
+  LOGOUT,
+  REGISTER,
+  EDIT,
+  NEW,
+  ONE,
+  ALL,
+  PRIVATE_PROFILE,
+} from './constants';
+import { apiUrl } from './urlHandler';
+
+const endpoints = {
+  // Projects routes
+  [PROJECTS]: {
+    [GET]: {
+      [ALL]: `${PROJECTS}`,
+      [ONE]: (meta) => `${PROJECTS}/${meta}`,
+    },
+    [POST]: {
+      [ONE]: `${PROJECTS}/${NEW}`,
+    },
+    [PATCH]: {
+      [ONE]: (meta) => `${PROJECTS}/${meta}/${EDIT}`,
+    },
+    [DELETE]: {
+      [ONE]: (meta) => `${PROJECTS}/${meta}/${DELETE}`,
+    },
+  },
+  // User routes
+  [USERS]: {
+    [POST]: {
+      [LOGIN]: `${USERS}/${LOGIN}`,
+    },
+    [POST]: {
+      [LOGOUT]: `${USERS}/${LOGOUT}`,
+    },
+    [POST]: {
+      [REGISTER]: `${USERS}/${REGISTER}`,
+    },
+    [GET]: {
+      [ALL]: `${USERS}`,
+      [ONE]: (meta) => `${USERS}/${meta}`,
+    },
+    [DELETE]: {
+      [ONE]: (meta) => `${USERS}/${meta}/${DELETE}`,
+    },
+    [GET]: {
+      [PRIVATE_PROFILE]: `${USERS}/me`,
+    },
+    [PATCH]: {
+      [PRIVATE_PROFILE]: `${USERS}/myprofile/${EDIT}`,
+    },
+  },
+  [CONTACT]: {
+    [POST]: {
+      [CONTACT]: `${CONTACT}`,
+    },
+  },
+};
+
+// get api endpoint from endoints{} list
+// eslint-disable-next-line import/prefer-default-export
+export const getEndpoint = (resource, method, type, meta) => {
+  try {
+    const endpoint = endpoints[resource][method][type];
+
+    return meta ? `/${endpoint(meta)}` : `/${endpoint}`;
+  }
+  catch (error) {
+    return apiUrl;
+  }
+};
