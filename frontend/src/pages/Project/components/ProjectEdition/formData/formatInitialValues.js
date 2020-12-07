fdd017a0f613297165cc48201c44941c148f09f5
Jordan MacBook <suarez.jordan@orange.fr>
2020-12-07 14:16:27 +0100

    WIP fix project edition/creation structure

--- frontend/src/pages/Project/components/ProjectEdition/formData/formatInitialValues.js
+++ frontend/src/pages/Project/components/ProjectEdition/formData/formatInitialValues.js
@@ -1,14 +1,42 @@
-export default (project) => ({
-  title: project.title,
-  githubLink: project.github_link,
-  projectLink: project.project_link,
-  description: project.description,
-  image: project.image,
-  imageName: 'Aucun fichier choisi',
-  tags: [
-    { name: 'React' },
-    { name: 'PHP' },
-    { name: 'Ruby' },
-  ],
-  partners: [],
-});
+import { get } from 'lodash';
+
+export default (project) => {
+  console.log(project);
+  const tags = [project.Tag];
+
+  const isNotNull = (object) => (object !== null ? object : false);
+  const addTag = (tag) => tags.push(tag);
+
+  if (isNotNull(project.Tag2)) {
+    addTag(project.Tag2);
+  }
+
+  if (isNotNull(project.Tag3)) {
+    addTag(project.Tag3);
+  }
+
+  if (isNotNull(project.Tag4)) {
+    addTag(project.Tag4);
+  }
+
+  if (isNotNull(project.Tag5)) {
+    addTag(project.Tag5);
+  }
+
+  if (isNotNull(project.Tag6)) {
+    addTag(project.Tag6);
+  }
+
+  console.log(tags);
+
+  return ({
+    title: project.title,
+    githubLink: project.github_link,
+    projectLink: project.project_link,
+    description: project.description,
+    image: project.image,
+    imageName: 'Aucun fichier choisi',
+    tags,
+    partners: [],
+  });
+};
