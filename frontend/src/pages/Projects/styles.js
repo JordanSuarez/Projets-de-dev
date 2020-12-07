389e44a9ed479ac171eb1733b321734b3c8ad4f3
GomezCeline <Celinegmz9@gmail.com>
2020-12-07 16:43:33 +0100

    CSS modifications

--- frontend/src/pages/Projects/styles.js
+++ frontend/src/pages/Projects/styles.js
@@ -1,4 +1,19 @@
 // eslint-disable-next-line no-unused-vars
 export default ({ palette }) => ({
+  container: {
+    maxWidth: '1200px',
+    margin: '0 auto',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  listCard: {
+    display: 'flex',
+  },
+  pagination: {
+    width: '255px',
+    margin: '1em auto',
+  },
 
 });
