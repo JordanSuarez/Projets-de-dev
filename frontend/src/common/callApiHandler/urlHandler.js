1c3334aa69d326e2c3c12c495801d7a086bf14d0
Ubuntu <ubuntu@ip-172-31-60-79.ec2.internal>
2020-12-15 17:22:14 +0000

    mise en prod

--- frontend/src/common/callApiHandler/urlHandler.js
+++ frontend/src/common/callApiHandler/urlHandler.js
@@ -7,7 +7,7 @@ const { REACT_APP_API_HOST } = process.env;
 const { REACT_APP_API_PORT } = process.env;
 
 // BASE URL of api, from .env file
-export const apiUrl = `${REACT_APP_API_PROTOCOL}://${REACT_APP_API_HOST}:${REACT_APP_API_PORT}/api`;
+export const apiUrl = `${REACT_APP_API_PROTOCOL}://${REACT_APP_API_HOST}/api`;
 
 // Create client instance for axios, send token if exist, else null
 // const axiosInstance = axios.create({
