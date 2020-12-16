142fd7b8ef32a4324a91dd783052dc8a44d0436f
GomezCeline <Celinegmz9@gmail.com>
2020-12-16 11:32:40 +0100

    test socketio

--- frontend/src/common/redux/middlewares/chat/index.js
+++ frontend/src/common/redux/middlewares/chat/index.js
@@ -13,9 +13,9 @@ let socket;
 const chatMiddleWare = (store) => (next) => (action) => {
   switch (action.type) {
     case CONNECT_WEBSOCKET:
-      socket = io('http://ec2-34-202-164-145.compute-1.amazonaws.com/api');
+      socket = io('http://ec2-34-202-164-145.compute-1.amazonaws.com');
       socket.on('send_message', (message) => {
-        // console.log('EMIT_MESSAGE middleware pass', message);
+        console.log('EMIT_MESSAGE middleware pass', message);
         store.dispatch(addMessage(message));
       });
       break;
